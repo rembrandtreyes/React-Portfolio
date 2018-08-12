ff5433c9a300f047e9eb0ee74e88918e6530133d
rembrandtreyes <reyes.rembrandt@gmail.com>
2018-08-12 00:08:01 -0400

    Initial commit for personal portfolio

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WebpackMd5Hash = require('webpack-md5-hash')
+
+module.exports = {
+  entry: { main: './src/index.js' },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[chunkhash].js'
+
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract(
+          {
+            fallback: 'style-loader',
+            use: ['css-loader', 'sass-loader']
+          })
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin(
+      {filename: 'style.[hash].css', disable: false, allChunks: true}
+    ),
+    new HtmlWebpackPlugin({
+      inject: false,
+      hash: true,
+      template: './src/index.html',
+      filename: 'index.html'
+    }),
+    new WebpackMd5Hash()
+  ]
+}
