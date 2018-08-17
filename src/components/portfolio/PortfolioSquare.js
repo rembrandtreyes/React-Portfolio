2685fb7f93f0d21a40a681cc0e48faa0eebc41f0
rembrandtreyes <reyes.rembrandt@gmail.com>
2018-08-16 21:12:05 -0400

    Added FA icons for the languages that the project uses

--- src/components/portfolio/PortfolioSquare.js
+++ src/components/portfolio/PortfolioSquare.js
@@ -12,13 +12,25 @@ class PortfolioSquare extends Component {
           title: 'Wiki-Viewer',
           url: 'https://codepen.io/rembrandtreyes/full/OjpVyw/',
           preview: require('../../../src/images/wiki-viewer.png'),
-          lang: [{langId: this.nextId(), icon: <FaHtml5 />}]
+          lang: [
+            {
+              langId: this.nextId(),
+              icons: [<FaHtml5 />]
+            }
+          ]
         },
         {
           id: this.nextId(),
           title: 'Technical Document',
           url: 'https://codepen.io/rembrandtreyes/pen/aKgrGj',
-          preview: require('../../../src/images/technical-document.png')
+          preview: require('../../../src/images/technical-document.png'),
+          lang: [
+            {
+              langId: this.nextId(),
+              icons: [<FaHtml5 />]
+            }
+          ]
+
         }
       ]
     }
@@ -37,7 +49,9 @@ class PortfolioSquare extends Component {
             <div key={square.id}>
             <p className="title">{square.title}</p>
             <img className="preview-width" src={square.preview} />
-            <span key={square.lang.langId}>{square.lang.icon}</span>
+            <div key={square.lang[0].langId}>
+              {square.lang[0].icons.map((icon, index) => {return <span key={index}>{icon}</span>})}
+            </div>
             <a href={square.url}>Link</a>
           </div>
           ])
